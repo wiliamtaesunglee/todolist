2fc88068a86a3bf3c422fd4974c6f4806d863fb9
wim <wiliam.tae@gmail.com>
2019-11-02 11:35:47 -0300

    provaider added to index on the root

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import { render } from "react-dom";
 import App from "./components/home/App";
 import reducer from "./redux-flow/reducers/todos";
 import { createStore } from 'redux'
-
+import { Provaider } from "react-redux"
 const store = createStore(reducer)
+console.log(store)
 
-render(<App />, document.getElementById("app"));
+render(
+  <Provaider store={store}>
+    <App />
+  </Provaider>
+, document.getElementById("app"));
