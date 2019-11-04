fd5ccc7b09666ff307a5c26ff210ce305c2cfc77
wim <wiliam.tae@gmail.com>
2019-11-04 19:28:24 -0200

    show list completed test

--- src/redux-flow/reducers/visibility-filter/visibility-filter.test.js
+++ src/redux-flow/reducers/visibility-filter/visibility-filter.test.js
@@ -17,3 +17,13 @@ it('should show all todos', () => {
   const after = 'SHOW_ALL'
   expect(visibilityFilter(before, action)).to.be.equal(after)
 })
+
+it('should show just completed todos', () => {
+  const before = 'SHOW_ALL'
+  const action = deepFreeze({
+    type: 'SET_VISIBILITY_FILTER',
+    payload: {filter: 'SHOW_COMPLETED'}
+  })
+  const after = 'SHOW_COMPLETED'
+  expect(visibilityFilter(before, action)).to.be.equal(after)
+})
