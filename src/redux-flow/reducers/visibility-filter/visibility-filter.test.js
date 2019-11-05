65ce06f0ac61e4762c59ee16c45a54c85d8ccff5
wim <wiliam.tae@gmail.com>
2019-11-05 06:09:44 -0200

    actions imported on visibility filter test file

--- src/redux-flow/reducers/visibility-filter/visibility-filter.test.js
+++ src/redux-flow/reducers/visibility-filter/visibility-filter.test.js
@@ -3,48 +3,49 @@
 import visibilityFilter, { initialState } from './index.js'
 import { expect } from 'chai'
 import deepFreeze from 'deep-freeze'
+import { SHOW_ALL, SET_VISIBILITY_FILTER, SHOW_COMPLETED, SHOW_ACTIVE } from './actions'
 
 it('visibility filter should be a function', () => {
  expect(visibilityFilter).to.be.a('function')
 })
 
 it('should show all todos', () => {
-  const before = 'SHOW_COMPLETED'
+  const before = SHOW_COMPLETED
   const action = deepFreeze({
-    type: 'SET_VISIBILITY_FILTER',
-    payload: {filter: 'SHOW_ALL'}
+    type: SET_VISIBILITY_FILTER,
+    payload: {filter: SHOW_ALL}
   })
-  const after = 'SHOW_ALL'
+  const after = SHOW_ALL
   expect(visibilityFilter(before, action)).to.be.equal(after)
 })
 
 it('should show just completed todos', () => {
-  const before = 'SHOW_ALL'
+  const before = SHOW_ALL
   const action = deepFreeze({
-    type: 'SET_VISIBILITY_FILTER',
-    payload: {filter: 'SHOW_COMPLETED'}
+    type: SET_VISIBILITY_FILTER,
+    payload: {filter: SHOW_COMPLETED}
   })
-  const after = 'SHOW_COMPLETED'
+  const after = SHOW_COMPLETED
   expect(visibilityFilter(before, action)).to.be.equal(after)
 })
 
 it('should show just active todos', () => {
-  const before = 'SHOW_COMPLETED'
+  const before = SHOW_COMPLETED
   const action = deepFreeze({
-    type: 'SET_VISIBILITY_FILTER',
-    payload: {filter: 'SHOW_ACTIVE'}
+    type: SET_VISIBILITY_FILTER,
+    payload: {filter: SHOW_ACTIVE}
   })
-  const after = 'SHOW_ACTIVE'
+  const after = SHOW_ACTIVE
   expect(visibilityFilter(before, action)).to.be.equal(after)
 })
 
 it('should return the lest state when the the action is uknown', () => {
-  const before = 'SHOW_COMPLETED'
+  const before = SHOW_COMPLETED
   const action = deepFreeze({
     type: 'UNKNOWN',
-    payload: {filter: 'SHOW_ACTIVE' }
+    payload: {filter: SHOW_ACTIVE }
   })
-  const after = 'SHOW_COMPLETED'
+  const after = SHOW_COMPLETED
   expect(visibilityFilter(before, action)).to.be.equal(after)
 })
 
