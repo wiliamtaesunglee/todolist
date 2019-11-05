906c2f248ed20448c14d0fa9b4722f3ba1510907
wim <wiliam.tae@gmail.com>
2019-11-05 19:21:20 -0200

    combine reducers added

--- src/redux-flow/reducers/index.js
+++ src/redux-flow/reducers/index.js
@@ -1,11 +1,17 @@
+'use strict'
+
+import { combineReducers } from 'redux'
 import reducerTodo from "./todos";
 import reducerVisibilityFilter from './visibility-filter';
 
-const rootReducer = (state = {}, action) => {
-  return {
-    todos: reducerTodo(state.todos, action),
-    visibilityFilter: reducerVisibilityFilter(state.visibilityFilter, action)
-  }
-}
+//const rootReducer = (state = {}, action) => {
+//  return {
+//    todos: reducerTodo(state.todos, action),
+//    visibilityFilter: reducerVisibilityFilter(state.visibilityFilter, action)
+//  }
+//}
 
-export default rootReducer
+export default combineReducers({
+  todos: reducerTodo,
+  visibilityFilter: reducerVisibilityFilter
+})
