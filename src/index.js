1ef534dd2ea21fd2e2c328b1729636e3f68330e0
wil.i.am <wiliam@wiliams-MacBook-Pro.local>
2019-11-05 08:14:29 -0300

    subscribe added

--- src/index.js
+++ src/index.js
@@ -4,8 +4,13 @@ import App from "./components/home/App";
 import reducer from "./redux-flow/reducers/todos";
 import { createStore } from 'redux'
 import { Provider } from "react-redux"
+
+
 const store = createStore(reducer)
-console.log(store)
+
+store.subscribe(() => {
+  console.log('state: ', store.getState())
+})
 
 render(
 <div>
