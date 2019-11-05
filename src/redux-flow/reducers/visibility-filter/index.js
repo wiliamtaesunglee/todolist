d2a4c158c27ee8d7f15d5d9d4ccb1cfe686321df
wim <wiliam.tae@gmail.com>
2019-11-05 06:03:13 -0200

    import actions on index file

--- src/redux-flow/reducers/visibility-filter/index.js
+++ src/redux-flow/reducers/visibility-filter/index.js
@@ -1,10 +1,12 @@
 'use strict'
 
-export const initialState = 'SHOW_ALL'
+import { SET_VISIBILITY_FILTER, SHOW_ALL } from './actions'
+
+export const initialState = SHOW_ALL
 
 const visibilityFilter = (state = initialState, action) => {
   switch(action.type) {
-  case 'SET_VISIBILITY_FILTER':
+  case SET_VISIBILITY_FILTER:
     return action.payload.filter
   default:
     return state
