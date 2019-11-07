52c8dbf1ebcd630c2c234381b33475c78f60a67f
wil.i.am <wiliam@wiliams-MacBook-Pro.local>
2019-11-07 10:18:32 -0300

    clear index action of redux-flow/reducers/index.js and adde the text "todos" in front of the actions

--- src/redux-flow/reducers/index.js
+++ src/redux-flow/reducers/index.js
@@ -4,13 +4,6 @@ import { combineReducers } from 'redux'
 import todos from "./todos";
 import visibilityFilter from './visibility-filter';
 
-//const rootReducer = (state = {}, action) => {
-//  return {
-//    todos: reducerTodo(state.todos, action),
-//    visibilityFilter: reducerVisibilityFilter(state.visibilityFilter, action)
-//  }
-//}
-
 export default combineReducers({
   todos,
   visibilityFilter
