8d698cd29896e6a7a3347c8d57fdfb43711be9ae
wil.i.am <wiliam@wiliams-MacBook-Pro.local>
2019-11-07 09:40:31 -0300

    filter component added

---
+++ src/components/filter/index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+import React from 'react'
+
+const Filter = () => (
+  <div>
+    <h3>Mostrar</h3>
+    <a href=''>Todos</a> | <a href=''>Finalizados</a> | <a href=''>A fazer</a>
+  </div>
+)
+
+export default Filter
--- src/components/home/App.js
+++ src/components/home/App.js
@@ -3,16 +3,14 @@
 import React from "react";
 import css from "./App.module.css";
 import Form from '../form/index'
-import TodosList from '../todos-list'
+import TodosList from '../todos-list';
+import Filter from '../filter/index';
 
 const App = ({todos, handleToggleTodo}) => (
   <div>
     <Form/>
     <TodosList/>
-    <div>
-      <h3>Mostrar</h3>
-      <a href="">Todos</a> | <a href="">Finalizados</a> | <a href="">A fazer</a>
-    </div>
+    <Filter/>
   </div>
 )
 
