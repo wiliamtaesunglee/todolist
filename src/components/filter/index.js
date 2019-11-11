97910ad61f156ccf6368011cc5a7c60915c925d1
wim <wiliam.tae@gmail.com>
2019-11-11 21:52:56 -0200

    activeFilter added

--- src/components/filter/index.js
+++ src/components/filter/index.js
@@ -3,18 +3,25 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import style from './Filter.module.css'
+import * as actions from '../../redux-flow/reducers/visibility-filter/actions'
 
-const Filter = () => (
+const Filter = ({activeFilter}) => (
   <div>
     <h3>Mostrar</h3>
     {filterItems.map(items => {
-      return <a href='' key={items} className={style.filterText}>{items}</a>
+
+    if (items.action === activeFilter) return <span key={items.action} className={style.filterText}>{items.label}</span>
+
+      return <a href='' key={items.action} className={style.filterText}>{items.label}</a>
     })}
-    <span href=''>Todos</span> | <a href=''>Finalizados</a> | <a href=''>A fazer</a>
   </div>
 )
 
-const filterItems = ['Todos', 'Finalizados', 'A fazer']
+const filterItems = [
+  { label: 'Todos', action: actions.SHOW_ALL },
+  { label: 'Finalizados', action: actions.SHOW_COMPLETED },
+  { label: 'A fazer', action: actions.SHOW_ACTIVE }
+]
 
 const mapStateToProps = (state) => ({
   activeFilter: state.visibilityFilter
