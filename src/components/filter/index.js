c7c5c3f4ce91d06d36e69bf83acbb59fe9746c6b
wim <wiliam.tae@gmail.com>
2019-11-11 21:17:27 -0200

    style module component added to filter

--- src/components/filter/index.js
+++ src/components/filter/index.js
@@ -2,12 +2,13 @@
 
 import React from 'react'
 import { connect } from 'react-redux'
+import style from './Filter.module.css'
 
 const Filter = () => (
   <div>
     <h3>Mostrar</h3>
     {filterItems.map(items => {
-      return <a href='' key={items}>{items}</a>
+      return <a href='' key={items} className={style.filterText}>{items}</a>
     })}
     <span href=''>Todos</span> | <a href=''>Finalizados</a> | <a href=''>A fazer</a>
   </div>
