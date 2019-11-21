3caef3ef66da9f56cdf382942eba39257fea3534
wil.i.am <wiliam.tae@gmail.com>
2019-11-21 08:53:05 -0300

    simplify the visibility filter index

--- src/redux-flow/reducers/visibility-filter/index.js
+++ src/redux-flow/reducers/visibility-filter/index.js
@@ -1,16 +1,12 @@
 'use strict'
 
 import { SET_VISIBILITY_FILTER, SHOW_ALL } from './actions'
+import createReducers from '../create-reducers'
 
 export const initialState = SHOW_ALL
 
-const visibilityFilter = (state = initialState, action) => {
-  switch(action.type) {
-  case SET_VISIBILITY_FILTER:
-    return action.payload.filter
-  default:
-    return state
-  }
-}
+const visibilityFilter = createReducers(initialState, {
+  [SET_VISIBILITY_FILTER]: (state, action) => action.payload.filter
+})
 
 export default visibilityFilter
