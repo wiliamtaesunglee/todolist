a7646626528120e5f227b9506fefd3b9208368bf
wil.i.am <wiliam.tae@gmail.com>
2019-11-21 08:27:40 -0300

    object propotype has own property error corrected

--- src/redux-flow/reducers/todos/index.js
+++ src/redux-flow/reducers/todos/index.js
@@ -4,11 +4,13 @@ import { ADD_TODO, TOGGLE_TODO } from './actions.js'
 
 export const initialValue = []
 
-const createReducer = (initialValue, handleActions) => {
-  return (state = initialValue, action) => {
-    console.log(handleActions.hasOwnProperty(action))
-  }
- }
+const createReducer = (initialValue, handleActions) =>
+  (state = initialValue, action) =>
+    (Object.prototype.hasOwnProperty.call(handleActions, action.type)) ?
+      handleActions[action.type](state, action) :
+       state
+
+
 
 const todos = createReducer(initialValue, {
   [ADD_TODO]: (state, action) => state.concat({
