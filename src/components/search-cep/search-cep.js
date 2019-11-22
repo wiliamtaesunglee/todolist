abdab140145842606c026ad4c123329d9cb2d8d5
wil.i.am <wiliam.tae@gmail.com>
2019-11-22 09:59:30 -0300

    search separate in presentation and container

---
+++ src/components/search-cep/search-cep.js
@@ -0,0 +1,35 @@
+'use strict'
+
+import React from 'react'
+import style from './SearchCep.module.css'
+
+const SearchCep = () => (
+  <div className={style.div}>
+    <form className={style.form}>
+      <input type='text' name='cep' />
+      <button type='submit'>Buscar endereço</button>
+    </form>
+
+    <table className={style.table}>
+      <thead className={style.thead}>
+        <tr>
+          <td>CEP</td>
+          <td>Endereço</td>
+          <td>Bairro</td>
+          <td>Cidade</td>
+          <td>Estado</td>
+        </tr>
+      </thead>
+
+      <tbody>
+        <td>05433020</td>
+        <td>Rua</td>
+        <td>Bairro</td>
+        <td>Cidade</td>
+        <td>Estado</td>
+      </tbody>
+    </table>
+  </div>
+)
+
+export default SearchCep
